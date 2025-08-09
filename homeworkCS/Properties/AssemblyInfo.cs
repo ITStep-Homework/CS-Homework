6ebf53eb3d30ea98fcac28b92c4eb966faa5ceab
VoixKz <karimtaevbeksultan@gmail.com>
2025-08-09 15:53:22 +0500

    renames done

--- homeworkCS/Properties/AssemblyInfo.cs
+++ homeworkCS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("HomeworkCs")]
+[assembly: AssemblyTitle("homeworkCS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HomeworkCs")]
+[assembly: AssemblyProduct("homeworkCS")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
